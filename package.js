7af908eb7325ead332b5a57b0456a26969b0eed5
pravdomil <pravdomil.toman@gmail.com>
2018-02-13 19:19:53 +0100

    package git

--- package.js
+++ package.js
@@ -1,3 +1,7 @@
+Package.describe({
+    git: "https://github.com/pravdomil/Meteor-React-ssr-and-csr-with-loadable-and-subscriptions.git"
+})
+
 Package.onUse(function (api) {
     api.use("ecmascript")
     api.mainModule("client.js", "client")
