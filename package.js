ecba9d8658d4159ae32445915bb33b24f125fbca
pravdomil <pravdomil.toman@gmail.com>
2018-02-13 19:52:36 +0100

    hmmm

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
     version: "0.0.0",
     name: "pravdomil:react-render",
-    summary: "React server side render and client side render for Meteor with react-loadable and Meteor.subscriptions in mind",
+    summary: "React server side render and client side render with react-loadable and Meteor.subscriptions in mind",
     git: "https://github.com/pravdomil/Meteor-React-ssr-and-csr-with-loadable-and-subscriptions.git",
 })
 
 Package.onUse(function (api) {
-    api.use("ecmascript")
+    api.use("ecmascript@0.10.3")
     api.mainModule("client.js", "client")
     api.mainModule("server.js", "server")
 })
