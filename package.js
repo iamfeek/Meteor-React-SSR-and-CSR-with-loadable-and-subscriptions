89f1079ba532c2c6cc89257553c9e705bb5d3a21
pravdomil <pravdomil.toman@gmail.com>
2018-02-13 19:24:13 +0100

    hmmm

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     version: "0.0.0",
-    git: "https://github.com/pravdomil/Meteor-React-ssr-and-csr-with-loadable-and-subscriptions.git"
+    git: "https://github.com/pravdomil/Meteor-React-ssr-and-csr-with-loadable-and-subscriptions.git",
 })
 
 Package.onUse(function (api) {
