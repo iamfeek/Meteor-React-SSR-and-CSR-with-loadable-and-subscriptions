631702809517eec087331145303405c768d88f26
pravdomil <pravdomil.toman@gmail.com>
2018-02-13 19:25:43 +0100

    name and summary

--- package.js
+++ package.js
@@ -1,5 +1,7 @@
 Package.describe({
     version: "0.0.0",
+    name: "pravdomil:react-render",
+    summary: "React server side render and client side render for Meteor with react-loadable and Meteor.subscriptions in mind",
     git: "https://github.com/pravdomil/Meteor-React-ssr-and-csr-with-loadable-and-subscriptions.git",
 })
 
